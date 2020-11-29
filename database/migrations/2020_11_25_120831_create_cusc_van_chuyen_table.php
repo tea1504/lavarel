6d19c5964fa4d0229abbfdeeb5144f923eefdfb1
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-11-29 10:58:22 +0700

    update

--- database/migrations/2020_11_25_120831_create_cusc_van_chuyen_table.php
+++ database/migrations/2020_11_25_120831_create_cusc_van_chuyen_table.php
@@ -16,16 +16,17 @@ class CreateCuscVanChuyenTable extends Migration
         Schema::create('cusc_van_chuyen', function (Blueprint $table) {
             $table->engine = 'InnoDB';
 
-            $table->unsignedTinyInteger('vc_ma')->autoIncrement();
+            $table->unsignedTinyInteger('vc_ma')->autoIncrement()->comment('Mã vận chuyển');
             $table->string('vc_ten', 200);
             $table->unsignedInteger('vc_chiphi')->default('0');
             $table->text('vc_diengiai');
             $table->timestamp('vc_taomoi')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('vc_capnhat')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->unsignedTinyInteger('vc_trangthai')->default('2');
-
+            
             $table->unique('vc_ten');
         });
+        DB::statement("ALTER TABLE `cusc_van_chuyen` comment 'Vận chuyển'");
     }
 
     /**
