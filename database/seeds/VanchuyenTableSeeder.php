fab7d622b5db20fcaa8da344f5490b46eeee91d7
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-12-01 19:19:19 +0700

    update

--- database/seeds/VanchuyenTableSeeder.php
+++ database/seeds/VanchuyenTableSeeder.php
@@ -12,6 +12,19 @@ class VanchuyenTableSeeder extends Seeder
     public function run()
     {
         $list = [];
+        $faker    = Faker\Factory::create('vi_VN');
+        $types = [];
+        for($i=1; $i<=count($types); $i++){
+            array_push($list,[
+                'vc_ma' => $i,
+                'vc_ten' => $types[$i-1],
+                'vc_chiphi' => $i,
+                'vc_diengiai' => $i,
+                'vc_taomoi' => $i,
+                'vc_capnhat' => $i,
+                'vc_trangthai' => 2
+            ]);
+        }
         DB::table('cusc_van_chuyen')->insert($list);
     }
 }
